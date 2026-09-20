b28e0c0bfe69bff8e0b010fa43b80b380654ac5a
Ishanvi <ishanvinaraharisetti@gmail.com>
2026-09-20 22:46:18 +0530

    Adding new user

--- scripts/set-admin-role.mjs
+++ scripts/set-admin-role.mjs
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const userId = 'b2e3cc1c-7475-4204-bd88-63a08e2a8a97';
+const userId = '68fcfb74-0160-4843-b5bf-20777f73bc37';
 const supabaseUrl = process.env.SUPABASE_URL;
 const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
